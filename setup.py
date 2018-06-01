eee05782d6458edd98a7c5f227c787d6b275084b
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2018-06-01 20:47:09 +0300

    Add support for Python 3

--- setup.py
+++ setup.py
@@ -22,6 +22,8 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
+    install_requires=['six']
 )
